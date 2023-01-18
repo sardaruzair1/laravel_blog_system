dd52407dd3fd113d47837d87f8ca2c1422a1d9b8
sardaruzair <sardar@gmail.com>
2023-01-18 14:52:27 +0500

    bog system in laravel

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -33,9 +33,9 @@
   <link href="{{ asset('assets/auth/plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css') }}" rel="stylesheet" />
   
   
-  
-  <link href="{{ asset('assets/auth/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
-  
+  {{-- here --}}
+ 
+  @yield('styles')
   
   
   <link href="{{ asset('assets/auth/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
@@ -975,236 +975,7 @@
       <!-- ====================================
       ——— PAGE WRAPPER
       ===================================== -->
-      <div class="page-wrapper">
-        
-          <!-- Header -->
-          <header class="main-header" id="header">
-            <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-              <!-- Sidebar toggle button -->
-              <button id="sidebar-toggler" class="sidebar-toggle">
-                <span class="sr-only">Toggle navigation</span>
-              </button>
-
-              <span class="page-title">dashboard</span>
-
-              <div class="navbar-right ">
-
-                <!-- search form -->
-                <div class="search-form">
-                  <form action="index.html" method="get">
-                    <div class="input-group input-group-sm" id="input-group-search">
-                      <input type="text" autocomplete="off" name="query" id="search-input" class="form-control" placeholder="Search..." />
-                      <div class="input-group-append">
-                        <button class="btn" type="button">/</button>
-                      </div>
-                    </div>
-                  </form>
-                  <ul class="dropdown-menu dropdown-menu-search">
-
-                    <li class="nav-item">
-                      <a class="nav-link" href="index.html">Morbi leo risus</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="index.html">Dapibus ac facilisis in</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="index.html">Porta ac consectetur ac</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="index.html">Vestibulum at eros</a>
-                    </li>
-
-                  </ul>
-
-                </div>
-
-                <ul class="nav navbar-nav">
-                  <!-- Offcanvas -->
-            
-                  <!-- User Account -->
-                  <li class="dropdown user-menu">
-                    <button class="dropdown-toggle nav-link" data-toggle="dropdown">
-                      <img src="{{ asset('assets/auth/images/user/user-xs-01.jpg') }}" class="user-image rounded-circle" alt="User Image" />
-                      <span class="d-none d-lg-inline-block">John Doe</span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-right">
-                      <li>
-                        <a class="dropdown-link-item" href="user-profile.html">
-                          <i class="mdi mdi-account-outline"></i>
-                          <span class="nav-text">My Profile</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-link-item" href="email-inbox.html">
-                          <i class="mdi mdi-email-outline"></i>
-                          <span class="nav-text">Message</span>
-                          <span class="badge badge-pill badge-primary">24</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-link-item" href="user-activities.html">
-                          <i class="mdi mdi-diamond-stone"></i>
-                          <span class="nav-text">Activitise</span></a>
-                      </li>
-                      <li>
-                        <a class="dropdown-link-item" href="user-account-settings.html">
-                          <i class="mdi mdi-settings"></i>
-                          <span class="nav-text">Account Setting</span>
-                        </a>
-                      </li>
-
-                      <li class="dropdown-footer">
-                        <a class="dropdown-link-item" href="sign-in.html"> <i class="mdi mdi-logout"></i> Log Out </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-
-
-          </header>
-
-        <!-- ====================================
-        ——— CONTENT WRAPPER
-        ===================================== -->
-        <div class="content-wrapper">
-          <div class="content">                
-                  <!-- Top Statistics -->
-                  <div class="row">
-                    <div class="col-xl-3 col-sm-6">
-                      <div class="card card-default card-mini">
-                        <div class="card-header">
-                          <h2>$18,699</h2>
-                          <div class="dropdown">
-                            <a class="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </div>
-                          <div class="sub-title">
-                            <span class="mr-1">Sales of this year</span> |
-                            <span class="mx-1">45%</span>
-                            <i class="mdi mdi-arrow-up-bold text-success"></i>
-                          </div>
-                        </div>
-                        <div class="card-body">
-                          <div class="chart-wrapper">
-                            <div>
-                              <div id="spline-area-1"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                      <div class="card card-default card-mini">
-                        <div class="card-header">
-                          <h2>$14,500</h2>
-                          <div class="dropdown">
-                            <a class="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </div>
-                          <div class="sub-title">
-                            <span class="mr-1">Expense of this year</span> |
-                            <span class="mx-1">50%</span>
-                            <i class="mdi mdi-arrow-down-bold text-danger"></i>
-                          </div>
-                        </div>
-                        <div class="card-body">
-                          <div class="chart-wrapper">
-                            <div>
-                              <div id="spline-area-2"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                      <div class="card card-default card-mini">
-                        <div class="card-header">
-                          <h2>$4199</h2>
-                          <div class="dropdown">
-                            <a class="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </div>
-                          <div class="sub-title">
-                            <span class="mr-1">Profit of this year</span> |
-                            <span class="mx-1">20%</span>
-                            <i class="mdi mdi-arrow-down-bold text-danger"></i>
-                          </div>
-                        </div>
-                        <div class="card-body">
-                          <div class="chart-wrapper">
-                            <div>
-                              <div id="spline-area-3"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-xl-3 col-sm-6">
-                      <div class="card card-default card-mini">
-                        <div class="card-header">
-                          <h2>$20,199</h2>
-                          <div class="dropdown">
-                            <a class="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </div>
-                          <div class="sub-title">
-                            <span class="mr-1">Revenue of this year</span> |
-                            <span class="mx-1">35%</span>
-                            <i class="mdi mdi-arrow-up-bold text-success"></i>
-                          </div>
-                        </div>
-                        <div class="card-body">
-                          <div class="chart-wrapper">
-                            <div>
-                              <div id="spline-area-4"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-
-          
-
-
-                
-                <!-- Table Product -->
-            
-
-
-          
-
-
-              <!-- Stock Modal -->
-           
-</div>
+      @yield('content')
           
         </div>
         
@@ -1226,72 +997,14 @@
     </div>
     
                     <!-- Card Offcanvas -->
-           
-
-
-
-    
                     <script src="{{ asset('assets/auth/plugins/jquery/jquery.min.js') }}"></script>
                     <script src="{{ asset('assets/auth/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
                     <script src="{{ asset('assets/auth/plugins/simplebar/simplebar.min.js') }}"></script>
                     <script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
-
-                    
-                    
-                    <script src="{{ asset('assets/auth/plugins/apexcharts/apexcharts.js') }}"></script>
-                    
-                    
-                    
-                    <script src="{{ asset('assets/auth/plugins/DataTables/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
-                    
-                    
-                    
-                    <script src="{{ asset('assets/auth/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
-                    <script src="{{ asset('assets/auth/plugins/jvectormap/jquery-jvectormap-world-mill.js') }}"></script>
-                    <script src="{{ asset('assets/auth/plugins/jvectormap/jquery-jvectormap-us-aea.js') }}"></script>
-                    
-                    
-                    
-                    <script src="{{ asset('assets/auth/plugins/daterangepicker/moment.min.js') }}"></script>
-                    <script src="{{ asset('assets/auth/plugins/daterangepicker/daterangepicker.js') }}"></script>
-                    <script>
-                      jQuery(document).ready(function() {
-                        jQuery('input[name="dateRange"]').daterangepicker({
-                        autoUpdateInput: false,
-                        singleDatePicker: true,
-                        locale: {
-                          cancelLabel: 'Clear'
-                        }
-                      });
-                        jQuery('input[name="dateRange"]').on('apply.daterangepicker', function (ev, picker) {
-                          jQuery(this).val(picker.startDate.format('MM/DD/YYYY'));
-                        });
-                        jQuery('input[name="dateRange"]').on('cancel.daterangepicker', function (ev, picker) {
-                          jQuery(this).val('');
-                        });
-                      });
-                    </script>
-                    
-                    
-                    
-                    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-                    
-                    
-                    
+                    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>                  
                     <script src="{{ asset('assets/auth/plugins/toaster/toastr.min.js') }}"></script>
-
-                    
-                    
                     <script src="{{ asset('assets/auth/js/mono.js') }}"></script>
-                    <script src="{{ asset('assets/auth/js/chart.js') }}"></script>
-                    <script src="{{ asset('assets/auth/js/map.js') }}"></script>
                     <script src="{{ asset('assets/auth/js/custom.js') }}"></script>
-
-                    
-
-
-                    <!--  -->
-
-
+                    @yield('scripts')
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/logout', function () {
+    auth()->logout();
 });
 
-Route::view('/theme', 'layouts.auth');
+Route::view('/theme', 'auth.dashboard');
 
-Route::view('/analytics', 'layouts.tst');
-Route::get('/analytics', function () {
-    return view('layouts.tst');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('dashboard',[DashboardController::class, 'dashboard'])->name('dashboard');
